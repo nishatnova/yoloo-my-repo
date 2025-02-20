e3c8809c3d8fc13f40d6a3a6b76e3e9d820863f1
Nishat Anzum Nova <nishat15-12132@diu.edu.bd>
2025-02-20 12:38:47 +0600

    env change and admin create

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // List of admins to create
+        $admins = [
+            [
+                'email' => 'nova8123@gmail.com',
+                'name' => 'Admin 1',
+                'password' => '123456789',
+                'role' => 'admin',
+            ],
+            [
+                'email' => 'sadikahmed2258@gmail.com',
+                'name' => 'Admin 2',
+                'password' => '123456789',
+                'role' => 'admin',
+            ],
+            [
+                'email' => 'demoad007@gmail.com',
+                'name' => 'Admin',
+                'password' => '123456789',
+                'role' => 'admin',
+            ],
+          
+        ];
+
+        foreach ($admins as $adminData) {
+            $admin = User::firstOrCreate(
+                ['email' => $adminData['email']],
+                [
+                    'name' => $adminData['name'],
+                    'password' => Hash::make($adminData['password']),
+                    'role' => $adminData['role'],
+                ]
+            );
+
+            if ($admin->wasRecentlyCreated) {
+                $this->command->info("Admin {$adminData['name']} created successfully.");
+            } else {
+                $this->command->info("Admin {$adminData['name']} already exists.");
+            }
+        }
+    }
+}
