ecfbdcf31c1736c70d101e93e0e2a177306c6990
Nishat Anzum Nova <nishat15-12132@diu.edu.bd>
2025-03-04 12:22:56 +0600

    package inquiries model

--- app/Models/PackageInquiry.php
+++ app/Models/PackageInquiry.php
@@ -7,8 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class PackageInquiry extends Model
 {
     protected $fillable = [
-        'name', 'email', 'phone', 'event_start_date', 'event_end_date', 'guests', 'event_type', 'package_id', 'status'
+        'user_id','package_id', 'name', 'email', 'phone', 'event_start_date', 'event_end_date', 'guests', 'event_type', 'status'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public function package()
     {
--- database/migrations/2025_03_04_054815_create_package_inquiries_table.php
+++ database/migrations/2025_03_04_054815_create_package_inquiries_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('package_inquiries', function (Blueprint $table) {
             $table->id();
             $table->foreignId('package_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); 
             $table->string('name');
             $table->string('email');
             $table->string('phone')->nullable();
