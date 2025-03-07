deaf589698de43b2ccbc548ae5ece801cd0e20dd
Nishat Nova <nishat15-12132@diu.edu.bd>
2025-03-07 21:41:39 +0600

    use trait for review

--- app/Http/Controllers/API/ReviewController.php
+++ app/Http/Controllers/API/ReviewController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Traits\ResponseTrait;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
-    //
+    use ResponseTrait;
 }
