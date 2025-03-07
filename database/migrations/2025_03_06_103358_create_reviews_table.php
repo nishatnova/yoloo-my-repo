83f93025180e876c7456d1de261512d0e88b2493
Nishat Nova <nishat15-12132@diu.edu.bd>
2025-03-07 21:22:26 +0600

    review table

--- database/migrations/2025_03_06_103358_create_reviews_table.php
+++ database/migrations/2025_03_06_103358_create_reviews_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('package_id')->constrained('packages')->onDelete('cascade');
+            $table->decimal('rating', 2, 1);
+            $table->text('comment');
             $table->timestamps();
         });
     }
