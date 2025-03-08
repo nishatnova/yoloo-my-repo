74bf421d29d5b6f2e0094b6f1d9381e5d5063fd2
Nishat Anzum Nova <nishat15-12132@diu.edu.bd>
2025-03-08 14:51:43 +0600

    assign staff model

--- app/Models/PackageInquireStaff.php
+++ app/Models/PackageInquireStaff.php
@@ -2,9 +2,35 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PackageInquireStaff extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'package_inquiry_id', 'photographer_application_id', 'decorator_application_id', 'catering_application_id'
+    ];
+
+    // Define relationships
+    public function packageInquiry()
+    {
+        return $this->belongsTo(PackageInquiry::class, 'package_inquiry_id');
+    }
+
+    public function photographerApplication()
+    {
+        return $this->belongsTo(JobApplication::class, 'photographer_application_id');
+    }
+
+    public function decoratorApplication()
+    {
+        return $this->belongsTo(JobApplication::class, 'decorator_application_id');
+    }
+
+    public function cateringApplication()
+    {
+        return $this->belongsTo(JobApplication::class, 'catering_application_id');
+    }
 }
