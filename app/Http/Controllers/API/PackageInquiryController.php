439a9aa25b03d84c1a19ec921fc5619d90ce5d8b
Nishat Anzum Nova <nishat15-12132@diu.edu.bd>
2025-03-24 15:59:17 +0600

    role change in show detail package inquire

--- app/Http/Controllers/API/PackageInquiryController.php
+++ app/Http/Controllers/API/PackageInquiryController.php
@@ -149,7 +149,7 @@ class PackageInquiryController extends Controller
                 }
                 if ($inquiry->packageInquireStaff->Application) {
                     $assignedStaff[] = [
-                        'role' => 'Decorator',
+                        'role' => 'Decorate',
                         'name' => $inquiry->packageInquireStaff->Application->applicant_name,
                         'id' => $inquiry->packageInquireStaff->Application->id,
                     ];
